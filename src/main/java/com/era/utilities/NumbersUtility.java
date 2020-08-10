de956d698f936601bbd7724ae5a4ab4cc25fea3c
davidtadeovargas <coritocorito@hotmail.com>
2020-08-10 03:21:42 -0600

    Varios ajustes
    -En la utilería de hibernate HibernateUtil agregar el método getDBEmpresasJDBCConnection y getLocalJDBCConnection para obtener la conexión nativa de sql de dbempresas
    
    y de la base de datos local
    -Arreglar el archivo POM para el proyecto era_jasperreports
    -Agregar la dependencia de maven de jasperreports al proyecto era_jasperreports
    -Terminar la lógica de codificación de la clase BaseReportGenerator
    -Codificación para una máxima reutilización del modulo de era_jasperreports
    -Cuando se genera una venta de ticket en el punto de venta que genere el PDF de venta
    -Agregar soporte en era_jasperreports para que maven pueda tomar todos los reportes de la carpeta de recursos "resources"
    -En la clase de utlidad de números NumbersUtility crear el método convertNumberToStringRepresentation para crear el importe con letra de cualquier número
    -En el archivo POM del proyecto era_jasperreports hacer corrección para que todos los archivos de recursos en este caso todosl los jrxml se exporten al archivo jar
    
    apropiadamente
    -Agregar al archivo pom.xml del proyecto era_jasperreports el plugin para que todos los archivos de jasperreports.jrxml estén visibles en el proyecto era_easyretail
    
    concentrador

--- src/main/java/com/era/utilities/NumbersUtility.java
+++ src/main/java/com/era/utilities/NumbersUtility.java
@@ -9,15 +9,73 @@ import com.era.utilities.exceptions.EmptyStringException;
 import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.util.Locale;
+import java.util.regex.Pattern;
 
 /**
  *
  * @author PC
  */
 public class NumbersUtility extends BaseUtility {
+
+    private final String[] UNIDADES = {"", "un ", "dos ", "tres ", "cuatro ", "cinco ", "seis ", "siete ", "ocho ", "nueve "};
+    private final String[] DECENAS = {"diez ", "once ", "doce ", "trece ", "catorce ", "quince ", "dieciseis ",
+        "diecisiete ", "dieciocho ", "diecinueve", "veinte ", "treinta ", "cuarenta ",
+        "cincuenta ", "sesenta ", "setenta ", "ochenta ", "noventa "};
+    private final String[] CENTENAS = {"", "ciento ", "doscientos ", "trecientos ", "cuatrocientos ", "quinientos ", "seiscientos ",
+        "setecientos ", "ochocientos ", "novecientos "};
     
     public String toMoneyFormat(final BigDecimal value) throws Exception {
         return toMoneyFormat(String.valueOf(value));
+    }
+    
+    public String convertNumberToStringRepresentation(String numero, final String simbol, final String coinCode, final boolean monedaNacional, final boolean mayusculas) {
+        
+        String terminacion = "";
+        if(monedaNacional){
+            terminacion = "M.N.";
+        }
+
+        BigDecimal num = new BigDecimal(numero.replace("$","").replace(",",""));
+        numero = num.setScale(2,BigDecimal.ROUND_HALF_EVEN).toString();
+        
+        String literal;
+        String parte_decimal;    
+        //si el numero utiliza (.) en lugar de (,) -> se reemplaza
+        numero = numero.replace(".", ",");
+        //si el numero no tiene parte decimal, se le agrega ,00
+        if(numero.indexOf(",")==-1){
+            numero = numero + ",00";
+        }
+        //se valida formato de entrada -> 0,00 y 999 999 999,00
+        if(Pattern.matches("\\d{1,9},\\d{1,6}", numero)) {
+            //se divide el numero 0000000,00 -> entero y decimal
+            String Num[] = numero.split(",");            
+            //de da formato al numero decimal
+            parte_decimal = coinCode + " ." + Num[1] + "/100 " + terminacion;
+            //se convierte el numero a literal
+            if(Integer.parseInt(Num[0]) == 0) {//si el valor es cero
+                literal = "cero ";
+            } else if(Integer.parseInt(Num[0]) > 999999) {//si es millon
+                literal = getMill(Num[0]);
+            } else if(Integer.parseInt(Num[0]) > 999) {//si es miles
+                literal = getMil(Num[0]);
+            } else if(Integer.parseInt(Num[0]) > 99) {//si es centena
+                literal = getCent(Num[0]);
+            } else if(Integer.parseInt(Num[0]) > 9) {//si es decena
+                literal = getDec(Num[0]);
+            } else {//sino unids -> 9
+                literal = getUnid(Num[0]);
+            }
+            //devuelve el resultado en mayusculas o minusculas
+            if(mayusculas) {
+                return (literal + parte_decimal).toUpperCase();
+            } else {
+                return (literal + parte_decimal);
+            }
+        } 
+        else {//error, no se puede convertir
+            return literal = null;
+        }
     }
     
     public String toMoneyFormat(final float value) throws Exception {
@@ -57,5 +115,87 @@ public class NumbersUtility extends BaseUtility {
 
         //Return the value
         return text;
+    }
+    
+    /*Obtiene los millones*/
+    private String getMill(String numero) 
+    { 
+        //se obtiene los miles
+        String miles = numero.substring(numero.length() - 6);
+        //se obtiene los millones
+        String millon = numero.substring(0, numero.length() - 6);
+        String n;
+        if(millon.length()>1){
+            n = getCent(millon) + "millones ";
+        }else{
+            n = getUnid(millon) + "millon ";
+        }
+        return n + getMil(miles);        
+    }
+    
+    
+    /*Obtiene las decenas*/
+    private String getDec(String num) 
+    {                        
+        int n = Integer.parseInt(num);
+        if(n < 10) 
+        {
+            return getUnid(num);
+        } 
+        else if(n > 19) 
+        {
+            String u = getUnid(num);
+            if(u.equals("")) { //para 20,30,40,50,60,70,80,90
+                return DECENAS[Integer.parseInt(num.substring(0, 1)) + 8];
+            } else {
+                return DECENAS[Integer.parseInt(num.substring(0, 1)) + 8] + "y " + u;
+            }
+        } 
+        else 
+        {
+            return DECENAS[n - 10];
+        }
+        
+    }/*Fin de private String getDec(String num) */
+        
+    /*Obtiene las centenas*/
+    private String getCent(String num) 
+    {
+        if( Integer.parseInt(num)>99 )
+        {
+            if(Integer.parseInt(num) == 100) {//caso especial
+                return " cien ";
+            } else {
+                 return CENTENAS[Integer.parseInt(num.substring(0, 1))] + getDec(num.substring(1));
+            } 
+        }
+        else
+        {
+            //se quita el 0 antes de convertir a decenas
+            return getDec(Integer.parseInt(num)+"");            
+        }                
+    }
+    
+    private String getMil(String numero) 
+    {
+        //obtiene las centenas
+        String c = numero.substring(numero.length() - 3);
+        //obtiene los miles
+        String m = numero.substring(0, numero.length() - 3);
+        String n="";
+        //se comprueba que miles tenga valor entero
+        if(Integer.parseInt(m) > 0) {
+            n = getCent(m);           
+            return n + "mil " + getCent(c);
+        } else {
+            return "" + getCent(c);
+        } 
+    }
+    
+    private String getUnid(String numero) 
+    {
+        //si tuviera algun 0 antes se lo quita -> 09 = 9 o 009=9
+        String num = numero.substring(numero.length() - 1);
+        return UNIDADES[Integer.parseInt(num)];
     }
 }
